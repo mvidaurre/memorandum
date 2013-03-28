d18606182496ed63c2b8230e0c0d15cbb5353668
Elias Vitaliano Vidaurre Davila <dj.vita.09@gmail.com>
2013-03-28 14:17:54 -0600

    Adding basic User Model and Group Scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Memorandum::Application.routes.draw do
+  resources :groups, except: :edit
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
---
+++ spec/factories.rb
@@ -0,0 +1,9 @@
+# This will guess the User class
+FactoryGirl.define do
+  factory :user do
+    first_name "Elias"
+    last_name  "Vidaurre"
+    email "dj.vita@gmail.com"
+    password "password"
+  end
+end
