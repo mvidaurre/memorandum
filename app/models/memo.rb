bcf7e12a1fea97717c30b7758d59be7397e8eea9
Elias Vitaliano Vidaurre Davila <dj.vita.09@gmail.com>
2013-05-01 19:13:43 -0500

    Validation of uniqueness of membership for group and presence of memo's fields

--- app/models/memo.rb
+++ app/models/memo.rb
@@ -3,5 +3,5 @@ class Memo < ActiveRecord::Base
   belongs_to :user
   has_many :user_read_memos
   attr_accessible :description, :due_date, :title, :group, :user, :user_read_memos_count
-
+	validates :description, :due_date, :title, :presence => true
 end
